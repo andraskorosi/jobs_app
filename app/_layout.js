78ba14650d549c3132a01fc652976ab46aca6cca
andraskorosi <andraskorosi1@gmail.com>
2023-06-15 13:06:56 +0200

    add custom fonts to layout; extend ui for index, ScreenHeaderBtn and Welcome

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,25 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'; // *everything
+ 
+// when the app is loading makes splash screen visible
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  })
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+      // only show homepage when fonts are loaded
+    } else return null
+  }, [fontsLoaded]) // similar to useEffect
+
+  return <Stack onLayout={onLayoutRootView} />;
 }
 
 export default Layout
